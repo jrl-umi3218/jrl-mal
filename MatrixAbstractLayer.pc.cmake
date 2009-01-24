c49b8eac82777f30e3111c9f3372bc2401513181
florent <Florent Lamiraux florent@laas.fr>
2009-01-24 11:21:03 +0900

    Fixed require field in .pc file.

--- MatrixAbstractLayer.pc.cmake
+++ MatrixAbstractLayer.pc.cmake
@@ -8,6 +8,6 @@ docdir=${install_pkg_prefix}/share/doc/${CMAKE_PROJECT_NAME}
 Name: ${PROJECT_NAME}
 Description: Macros for versatile matrix and vector definitions and operations
 Version: ${PROJECT_VERSION}
-Requires: ${PROJECT_REQUIREMENTS}
+Requires: ${PACKAGE_REQUIREMENTS}
 Libs:   ${LIBDIR_KW}${install_pkg_libdir} ${${${PROJECT_NAME}}_LDFLAGS}
 Cflags: -I${install_pkg_include_dir} ${${${PROJECT_NAME}}_CFLAGS}
