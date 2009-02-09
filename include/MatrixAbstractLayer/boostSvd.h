7609a0d058ca5f72963e722ce64d5ef66d995593
stasse <stasse@stasse-laptop.(none)>
2009-02-10 08:27:34 +0900

    Removed ALL the references to jrlgesvd.f Some lazy guy did only half of the job...

--- include/MatrixAbstractLayer/boostSvd.h
+++ include/MatrixAbstractLayer/boostSvd.h
@@ -22,17 +22,6 @@
 #define __MAAL_BOOST_SVD_
 
 
-#ifdef WITH_OPENHRP
-
-extern "C"
-{
-  void jrlgesvd_(char const* jobu, char const* jobvt,
-	       int const* m, int const* n, double* a, int const* lda,
-	       double* s, double* u, int const* ldu,
-	       double* vt, int const* ldvt,
-	       double* work, int const* lwork, int* info);
-}
-#endif // #ifdef WITH_OPENHRP
 
 
 #endif // #ifndef __MAAL_BOOST_SVD_
