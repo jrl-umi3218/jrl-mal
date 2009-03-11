bc796d64aa030f79b95a8af1eb37cb3de995eae2
Francois Keith <francois.keith@aist.go.jp>
2009-03-11 14:04:01 +0100

    Remove a wrong \#include

--- unitTesting/TimeUtilsWindows.h
+++ unitTesting/TimeUtilsWindows.h
@@ -1,8 +1,6 @@
 /*! \file TimeUtilsWindows.h
 */
 
-#include <walkGenJrl/walkGenJrl_API.h>
-
 #ifdef WIN32
 
 #ifndef TIME_UTILS_WINDOWS_H
@@ -16,7 +14,7 @@ struct timezone
   int  tz_dsttime;     /* type of dst correction */
 };
 
-int WALK_GEN_JRL_EXPORT gettimeofday(struct timeval *tv, struct timezone *tz);
+int gettimeofday(struct timeval *tv, struct timezone *tz);
 
 #endif /*TIME_UTILS_WINDOWS_H*/
 
