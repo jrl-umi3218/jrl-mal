873b383e2015393603d9877ca0b17fdc790e23d9
florent <florent@laas.fr>
2010-01-20 10:43:20 +0100

    Remove deprecated directives in headers.
    	* include/MatrixAbstractLayer/abstract.h,
    	* include/MatrixAbstractLayer/abstractMatrix.h,
    	* include/MatrixAbstractLayer/abstractVector.h,
    	* include/MatrixAbstractLayer/boost.h,
    	* include/MatrixAbstractLayer/boostMacros.h,
    	* include/MatrixAbstractLayer/boostMatrix.h,
    	* include/MatrixAbstractLayer/boostMatrixSvd.h,
    	* include/MatrixAbstractLayer/boostSvd.h,
    	* include/MatrixAbstractLayer/boostVector.h.

--- include/MatrixAbstractLayer/abstractVector.h
+++ include/MatrixAbstractLayer/abstractVector.h
@@ -21,12 +21,6 @@
 #ifndef __MAAL_ABSTRACT_VECTOR_
 #define __MAAL_ABSTRACT_VECTOR_
 
-#ifdef WIN32
-#pragma message ( " -- !!! -- Warning : deprecated header file. Please consider using boost instead." )
-#else
-#warning "deprecated header file. Please consider using boost instead."
-#endif /* WIN32 */
-
 #include <MatrixAbstractLayer/abstractMacros.h>
 
 namespace maal{  namespace abstract {
--- include/MatrixAbstractLayer/boostSvd.h
+++ include/MatrixAbstractLayer/boostSvd.h
@@ -21,10 +21,4 @@
 #ifndef __MAAL_BOOST_SVD_
 #define __MAAL_BOOST_SVD_
 
-#ifdef WIN32
-#pragma message ( " -- !!! -- Warning : deprecated header file. Please consider using boost instead." )
-#else
-#warning "deprecated header file. Please consider using boost instead."
-#endif /* WIN32 */
-
 #endif // #ifndef __MAAL_BOOST_SVD_
