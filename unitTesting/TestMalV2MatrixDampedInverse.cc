445fdf16a3eeda3bb9f59ad3ef23088adc82e4d5
Maxime REIS <maxime.reis@gmail.com>
2011-05-10 16:22:00 +0200

    Add header in mal test files.

--- unitTesting/TestMalV2MatrixDampedInverse.cc
+++ unitTesting/TestMalV2MatrixDampedInverse.cc
@@ -1,6 +1,21 @@
+// Copyright 2011 Maxime Reis.
+//
+// This file is part of jrl-mal.
+// jrl-mal is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// jrl-mal is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// You should have received a copy of the GNU Lesser General Public License
+// along with jrl-mal.  If not, see <http://www.gnu.org/licenses/>.
+
 /* 
-Test Matrix class 
-*/
+ * Test Matrix class
+ */
 
 #include "assertion.hh"
 #include "malv2utils.hh"
@@ -60,7 +75,7 @@ void run_test()
   JRL_MAL_ASSERT(isMatrixNull(tmp)
               && "test matrix damped inverse property for a non-square matrix #2: (invM*M)T = invM*M");
 
-  std::cout << "Matrix damped-inverse test has succeeded." << std::endl;
+  std::cout << "Mal v2 : Matrix damped-inverse test has succeeded." << std::endl;
 }
 
 GENERATE_TEST()
--- unitTesting/TestMalV2MatrixSetIdentity.cc
+++ unitTesting/TestMalV2MatrixSetIdentity.cc
@@ -1,6 +1,21 @@
+// Copyright 2011 Maxime Reis.
+//
+// This file is part of jrl-mal.
+// jrl-mal is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// jrl-mal is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// You should have received a copy of the GNU Lesser General Public License
+// along with jrl-mal.  If not, see <http://www.gnu.org/licenses/>.
+
 /* 
-Test Matrix class
-*/
+ * Test Matrix class
+ */
 
 #include "assertion.hh"
 #include "malv2utils.hh"
@@ -32,7 +47,7 @@ void run_test()
               && "test set non-square matrix to identity matrix #2");
 
   // All assertions true, test is passed
-  std::cout << "Matrix set to identity test has succeeded." << std::endl;
+  std::cout << "Mal v2 : Matrix set to identity test has succeeded." << std::endl;
 }
 
 GENERATE_TEST()
