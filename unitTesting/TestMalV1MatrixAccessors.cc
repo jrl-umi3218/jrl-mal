d5d16ca58a4b15ab646acca92101e9aaa5d4b87f
Maxime REIS <maxime.reis@gmail.com>
2011-05-10 16:22:00 +0200

    Add header in mal test files.

--- unitTesting/TestMalV1MatrixAccessors.cc
+++ unitTesting/TestMalV1MatrixAccessors.cc
@@ -1,6 +1,21 @@
+// Copyright 2011 Maxime Reis.
+//
+// This file is part of jrl-mal.
+// jrl-mal is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// jrl-mal is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// You should have received a copy of the GNU Lesser General Public License
+// along with jrl-mal.  If not, see <http://www.gnu.org/licenses/>.
+
 /* 
-Test Matrix macros
-*/
+ * Test Matrix macros
+ */
 
 #include "assertion.hh"
 #include "malv1utils.hh"
@@ -49,7 +64,7 @@ void run_test()
               && "correct sub-matrix extraction #3");
 
   // All assertions true, test is passed
-  std::cout << "Matrix accessors test has succeeded." << std::endl;
+  std::cout << "Mal v1 : Matrix accessors test has succeeded." << std::endl;
 }
 
 GENERATE_TEST()
