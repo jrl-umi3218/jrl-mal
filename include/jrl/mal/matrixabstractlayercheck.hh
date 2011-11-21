b71d635578dfc4424749137880868ded96a2a25f
Antonio El Khoury <aelkhour@laas.fr>
2011-11-21 17:14:33 +0100

    Ensure portability of isnan using boost::math version and add test.
    This hopefully avoids testing OS version to call right function.
    
         * include/jrl/mal/matrixabstractlayercheck.hh
         * tests/simple.cc

--- include/jrl/mal/matrixabstractlayercheck.hh
+++ include/jrl/mal/matrixabstractlayercheck.hh
@@ -24,22 +24,15 @@
 #ifndef MATRIXABSTRACTLAYER_CHECK_H
 # define MATRIXABSTRACTLAYER_CHECK_H
 
+#include <boost/math/special_functions/fpclassify.hpp>
+
 #include <jrl/mal/matrixabstractlayerboost.hh>
 
-//define isnan for win32 and mac users
-# ifdef WIN32
-#  ifndef isnan
-#   include <float.h>
-#   define isnan _isnan
-#  endif /*isnan*/
-# elif defined __APPLE__
-   inline bool isnan(double x) { return x != x; }
-# endif /*WIN32*/
 
 inline bool malIsNanVector(const vectorN& inVector)
 {
   for (unsigned int i=0; i<MAL_VECTOR_SIZE(inVector); i++) {
-    if (isnan(inVector[i])) return true;
+    if (boost::math::isnan(inVector[i])) return true;
   }
   return false;
 }
@@ -48,7 +41,7 @@ inline bool malIsNanMatrix(const matrixNxP& inMatrix)
 {
   for (unsigned int i=0; i<MAL_MATRIX_NB_ROWS(inMatrix); i++) {
     for (unsigned int j=0; i<MAL_MATRIX_NB_COLS(inMatrix); i++) {
-      if (isnan(inMatrix(i,j))) return true;
+      if (boost::math::isnan(inMatrix(i,j))) return true;
     }
   }
   return false;
